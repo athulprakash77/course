958f92ed3965c0ef51c179cb6ed1da949d630277
athulprakash007 <athull362@gmail.com>
2022-11-27 13:47:06 -0800

    finish

---
+++ src/app/addcourse/addcourse.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addcourse',
+  templateUrl: './addcourse.component.html',
+  styleUrls: ['./addcourse.component.css']
+})
+export class AddcourseComponent implements OnInit {
+
+
+  constructor() { }
+
+  courseTitle=""
+  courseDescription=""
+  courseDuration=""
+  courseDate=""
+  courseVenue=""
+
+  ngOnInit(): void {
+  }
+
+
+  read=()=>
+  {
+let data=
+    {
+      "courseTitle":this.courseTitle,
+    "courseDescription":this.courseDescription,
+    "courseDuration":this.courseDuration,
+    "courseDate":this.courseDate,
+    "courseVenue":this.courseVenue
+  }
+
+console.log(data)
+
+  }
+
+  
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddcourseComponent } from './addcourse/addcourse.component';
+import { ViewallcourseComponent } from './viewallcourse/viewallcourse.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const myroutes=[
+
+{path:"",component:AddcourseComponent},
+{path:"a",component:ViewallcourseComponent}
+
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddcourseComponent,
+    ViewallcourseComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  RouterModule.forRoot(myroutes),
+  FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
